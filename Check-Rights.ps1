4b4ed3bfdd00c400fed7f8c401a83e334c70dcf5
Zewwy <zew@live.ca>
2020-03-03 22:17:40 -0600

    inital script

---
+++ Check-Rights.ps1
@@ -0,0 +1,15 @@
+﻿$folder = "c:\temp\None"
+
+$CP = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$UGM = @($CP.Identities.Groups | ?{$_.AccountDomainSid -ne $null})
+$UGM = $UGM + $CP.Identities.User
+if(([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){$UGM = $UGM + ($CP.Identities.Groups | ?{$_.Value -eq "S-1-5-32-544"})}
+
+$TPP = (Get-Acl $folder).Access
+
+foreach ($P in $TPP.IdentityReference.Translate([System.Security.Principal.SecurityIdentifier]))
+{
+    foreach ($UP in $UGM){if($UP -eq $P){Write-Host "We got permissions here!";$TPP | ?{$_.IdentityReference.Translate([System.Security.Principal.SecurityIdentifier]) -match $P}}}
+}
+
+foreach($pu in (Get-LocalGroup Administrators | Get-LocalGroupMember).SID){if($pu -eq $CP.Identities.User -And !([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){Write-Host "You are an admin, but you are not elevated..."}}
